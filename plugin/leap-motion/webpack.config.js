527dd2c791d42071e449ca814d3c39f29522dc36
xteeven <david7246@gmail.com>
2018-10-27 16:17:53 -0300

    Leap motion - timer - peaker notes

---
+++ plugin/leap-motion/webpack.config.js
@@ -0,0 +1,44 @@
+const leapjsVersion = JSON.stringify(require('./node_modules/leapjs/package.json').version);
+const version = JSON.stringify(require('./package.json').version);
+const webpack = require('webpack');
+
+const banner = `
+  reveal-leap-motion v${version}
+  https://github.com/gneatgeek/reveal-leap-motion
+
+  Copyright 2016 Rory Hardy, James Sun, and other contributors
+  Released under the Apache-2.0 license
+  https://github.com/gneatgeek/reveal-leap-motion/blob/master/LICENSE
+
+  ===================================================================
+
+  LeapJS v${leapjsVersion}
+  http://github.com/leapmotion/leapjs/
+
+  Copyright 2013 LeapMotion, Inc. and other contributors
+  Released under the Apache-2.0 license
+  http://github.com/leapmotion/leapjs/blob/master/LICENSE.txt
+`.trim().replace(/^ +|"/gm, '');
+
+module.exports = {
+  entry: {
+    'reveal-leap-motion': './src',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        test: /\.js$/,
+      },
+    ],
+  },
+  output: {
+    filename: '[name].min.js',
+    path: __dirname,
+  },
+  plugins: [
+    new webpack.BannerPlugin(banner),
+  ],
+};
